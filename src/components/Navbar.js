5de8cd46a91f2d389e0acb3620e532a8edfa1478
Benjamin Powell <benjaminpowell24@gmail.com>
2024-01-14 21:31:07 +0000

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className='nav-center'>
-        <h3>redux toolkit</h3>
+        <h3>enter logo</h3>
 
         <div className='nav-container'>
           <CartIcon />
